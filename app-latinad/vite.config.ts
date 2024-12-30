2b3d6e36f3d09e3602523d6149ba2b5c09ef7d24
Octavio Martinez <omartinez1618@gmail.com>
2024-12-30 17:20:08 -0400

    feat: Agregar logotipos e implementar plugin SVGR
    Se agregaron los archivos SVG 'latinad.imagotipo.svg' y 'latinad.isotipo.svg'  con los logotipos de Latinad.  Además, se añadió el plugin 'vite-plugin-svgr' para soportar la importación directa de componentes SVG. Se actualizó la configuración de 'tsconfig.app.json' para incluir la declaración de tipos 'index.d.ts' y se agregó un alias para la ruta 'src' en 'vite.config.ts' para facilitar la importación de componentes.
    
    [by Kommiter]

--- app-latinad/vite.config.ts
+++ app-latinad/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import svgr from 'vite-plugin-svgr'
+import { resolve } from 'node:path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [svgr(),react()],
+  resolve: {
+    alias:{
+      "@": resolve(__dirname, "src")
+    }
+  }
 })
