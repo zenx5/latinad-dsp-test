72bb9238063102b5dd578806a04204bcf6e05a5e
Octavio Martinez <omartinez1618@gmail.com>
2024-12-30 15:49:59 -0400

    feat: Implementar diseño con Tailwind CSS y Ant Design
    Este commit añade Tailwind CSS y Ant Design a la aplicación.  Se eliminaron los componentes y estilos previos, reemplazándolos con una estructura básica usando Tailwind CSS.  Se incluye una configuración básica de enrutamiento con React Router.  El componente principal de la aplicación ahora es 'Main.tsx'.
    
    [by Kommiter]

--- app-latinad/src/main.tsx
+++ app-latinad/src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from "react-router";
+
 import './index.css'
-import App from './App.tsx'
+import Main from './views/Main.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/cart" element={<span>cart</span>} />
+        <Route path="/checkout" element={<span>checkout</span>} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
