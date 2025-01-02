4af629a33c0d7f04d00ccff9d0d108ad87de9293
Octavio Martinez <omartinez1618@gmail.com>
2025-01-02 08:21:28 -0400

    titles

--- app-latinad/src/translates/langs/es.ts
+++ app-latinad/src/translates/langs/es.ts
@@ -1,5 +1,6 @@
 export const es = {
     translation: {
-        title:'Titulo'
+        'title-action':'Pantallas disponibles en tu zona',
+        'copy-action':'No pierdas la oportunidad de crezcer con nosotros, mira aquí',
     }
 }
