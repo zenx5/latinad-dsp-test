671dde069126a615027d99577b2b882e2a6c5297
Octavio Martinez <omartinez1618@gmail.com>
2025-01-02 07:19:00 -0400

    feat: Implementar componente de búsqueda
    Este commit añade un nuevo componente de búsqueda a la aplicación.  Incluye un nuevo componente 'InputLocation' para la entrada de ubicación y actualiza el componente 'FormSearch' para usar este nuevo componente, además de mejorar su diseño y responsividad.  También se han añadido estilos para mejorar la apariencia del formulario de búsqueda.
    
    [by Kommiter]

--- app-latinad/src/components/FormSearch/FormSearch.tsx
+++ app-latinad/src/components/FormSearch/FormSearch.tsx
@@ -1,22 +1,18 @@
 import { Button, Input } from "antd";
 
 import "./FormSearch.css"
+import InputLocation from "../InputLocation/InputLocation";
 
 export default function FormSearch() {
 
-    return <form className="corners flex flex-col gap-5 w-1/3 py-8 px-4 bg-white rounded-tl-xl rounded-br-xl">
-        <Input size="large" prefix={
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
-            </svg>
-        }/>
-        <Input size="large" prefix={
+    return <form className="corners relative flex flex-col items-center gap-2 sm:gap-5 w-full sm:w-1/2 md:w-1/3 py-8 px-4 bg-white sm:rounded-tl-xl sm:rounded-br-xl rounded-none bottom-0">
+        <InputLocation />
+        <Input size="large" className="w-11/12" prefix={
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5" />
             </svg>
         }/>
-        <Input size="large" prefix={
+        <Input size="large" className="w-11/12" prefix={
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5" />
             </svg>
--- app-latinad/src/views/Main.tsx
+++ app-latinad/src/views/Main.tsx
@@ -1,8 +1,12 @@
+import FormSearch from '../components/FormSearch/FormSearch'
 import '../translates/i18n'
 import { useTranslation } from 'react-i18next'
 
 export default function Main() {
     const { t } = useTranslation()
 
-    return <main>{t('title')}</main>
+    return <main className="sm:p-4 p-0">
+        <h1>{t('title')}</h1>
+        <FormSearch />
+    </main>
 }
