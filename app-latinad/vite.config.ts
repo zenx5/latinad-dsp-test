dfe74960b988a5b6000099d509d38635375c0d58
Octavio Martinez <omartinez1618@gmail.com>
2025-01-13 23:53:24 -0400

    build: Actualiza dependencias y configuración de TypeScript
    Se actualizaron las dependencias '@types/node' y se cambió la configuración de TypeScript para usar ES2022 y agregar 'types: ['node']' a 'tsconfig.app.json'.  También se actualizó la configuración de paths en 'vite.config.ts' para usar 'fileURLToPath' y 'dirname' para compatibilidad.
    
    [by Kommiter]

--- app-latinad/vite.config.ts
+++ app-latinad/vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import svgr from 'vite-plugin-svgr'
-import { resolve } from 'node:path'
+import { fileURLToPath } from 'node:url';
+import { resolve, dirname } from 'node:path'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
